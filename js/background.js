46ae033e5c27335c765e8165d6dda68487fbf896
Omurbek Kurmanbekov <kurmanbekovomurbek80@gmail.com>
2023-12-13 23:56:08 +0600

    add some details

--- js/background.js
+++ js/background.js
@@ -1,36 +1,33 @@
-
 chrome.runtime.onInstalled.addListener(async () => {
     let url = chrome.runtime.getURL("html/hello.html");
-    let tab = await chrome.tabs.create({ url });
+    let tab = await chrome.tabs.create({url});
 
     chrome.storage.sync.get(['showClock'], (result) => {
         if (result.showClock) {
-            chrome.action.setBadgeText({ text: 'ON' });
+            chrome.action.setBadgeText({text: 'ON'});
         }
     });
 
     chrome.storage.sync.get(['timer'], (result) => {
         console.log('result', result)
         if (!result.timer) {
-            chrome.storage.sync.set({ 'timer': 1 })
+            chrome.storage.sync.set({'timer': 1})
         }
     });
 });
 
-chrome.commands.onCommand.addListener( (command) => {
+chrome.commands.onCommand.addListener((command) => {
 
     chrome.tabs.update({}, async (tab) => {
         debugger
         if (command == 'pin-current-tab') {
-            chrome.tabs.update({ pinned: !tab.pinned });
+            chrome.tabs.update({pinned: !tab.pinned});
         } else if (command == 'move-to-first') {
-            chrome.tabs.move(tab.id, { index: 0 });
-        }
-        else if (command == 'move-to-last') {
+            chrome.tabs.move(tab.id, {index: 0});
+        } else if (command == 'move-to-last') {
             const allTabs = await chrome.tabs.query({})
-            chrome.tabs.move(tab.id, { index: allTabs.length - 1 });
-        }
-        else if (command == 'copy-current-tab') {
+            chrome.tabs.move(tab.id, {index: allTabs.length - 1});
+        } else if (command == 'copy-current-tab') {
             chrome.tabs.duplicate(tab.id);
         }
     });
@@ -43,9 +40,9 @@ chrome.alarms.onAlarm.addListener((alarm) => {
         type: 'basic',
         iconUrl: '/icons/alert.png',
         title: 'Напоминашка',
-        message: 'Ахтунг! Пришло время напомнить!',
+        message: 'Время пришло!',
         buttons: [
-            { title: 'Спасибо' }
+            {title: 'Спасибо'}
         ],
         priority: 0
     }, (e) => {
@@ -54,13 +51,13 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 });
 
 chrome.notifications.onButtonClicked.addListener(async (notificationId, buttonIndex) => {
-    console.log({ notificationId, buttonIndex })
+    console.log({notificationId, buttonIndex})
 });
 
 chrome.notifications.onClicked.addListener(async (notificationId) => {
     chrome.storage.sync.get(['timer'], (result) => {
         if (result.timer) {
-            chrome.alarms.create({ delayInMinutes: result.timer });
+            chrome.alarms.create({delayInMinutes: result.timer});
         }
     })
 
